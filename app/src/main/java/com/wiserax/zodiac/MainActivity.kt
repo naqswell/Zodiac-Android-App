625ce1015cac47f519b99dcc368c98f06353f178
Naqswell <naqswell@gmail.com>
2021-11-22 17:30:37 +0300

    navigation minor fix

--- app/src/main/java/com/wiserax/zodiac/MainActivity.kt
+++ app/src/main/java/com/wiserax/zodiac/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : AppCompatActivity(), DateFragment.Callbacks, BirthDateFragm
     }
 
     override fun onDateButtonPressed() {
-        findNavController(R.id.nav_host_fragment_activity_main).navigate(HoroscopeFragmentDirections.actionHoroscopeToBirthdate())
+        findNavController(R.id.nav_host_fragment_activity_main).navigate(R.id.navigation_birthdate)
     }
 
     override fun onDateSet() {
